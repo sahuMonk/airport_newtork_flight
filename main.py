f30971b0fbe2bbd322fc5fc1005cb92417eb4718
Mayank Sahu <103450153+sahuMonk@users.noreply.github.com>
2023-04-04 23:18:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///votrbase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-# app.config['MAIL_USERNAME'] = ""
-# app.config['MAIL_PASSWORD'] = ""
+app.config['MAIL_USERNAME'] = ""
+app.config['MAIL_PASSWORD'] = ""
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
